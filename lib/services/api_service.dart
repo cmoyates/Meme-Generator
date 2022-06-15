f7dcf771e42d09d731c11960c6b9368eb2465a05
cmoyates <cmoyates@gmail.com>
2022-06-15 19:48:23 -0230

    Updated API

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -8,14 +8,14 @@ import 'package:url_launcher/url_launcher.dart';
 import 'dart:async';
 
 class APIImage extends StatelessWidget {
-  
   final String imgURL;
   final String topText;
   final String bottomText;
   final bool deepFried;
   final Color color;
 
-  APIImage({this.imgURL, this.topText, this.bottomText, this.deepFried, this.color});
+  APIImage(
+      {this.imgURL, this.topText, this.bottomText, this.deepFried, this.color});
 
   _launchURL(String url) async {
     if (await canLaunch(url)) {
@@ -27,33 +27,38 @@ class APIImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final params = {
+      "url": imgURL,
+      "top": topText,
+      "bottom": bottomText,
+      "deep_fry": deepFried ? "1" : "0"
+    };
 
-    String dfString = deepFried ? "1" : "0";
-    String params = imgURL + "|" + topText + "|" + bottomText + "|" + dfString;
+    final String _baseURL = "simple-meme-generator-api.herokuapp.com";
 
-    final String _baseURL = "https://fast-scrubland-86526.herokuapp.com/api?query=";
+    final uri = Uri.https(_baseURL, "/api", params);
 
-    String completeURL = _baseURL + params;
-
-    Image img = Image.network(completeURL);
+    Image img = Image.network(uri.toString());
 
     return Container(
       color: color,
-      child: Row(children: <Widget>[
-        img,
-        SizedBox(width: 20.0),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            //Text(memeText[1]["Upper Text"]),
-            IconButton(
-              icon: Icon(Icons.save_alt),
-              onPressed: () {
-                _launchURL(completeURL);
-            })
-          ],),
-      ],
-      mainAxisAlignment: MainAxisAlignment.center,
+      child: Row(
+        children: <Widget>[
+          img,
+          SizedBox(width: 20.0),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              //Text(memeText[1]["Upper Text"]),
+              IconButton(
+                  icon: Icon(Icons.save_alt),
+                  onPressed: () {
+                    _launchURL(uri.toString());
+                  })
+            ],
+          ),
+        ],
+        mainAxisAlignment: MainAxisAlignment.center,
       ),
     );
   }
